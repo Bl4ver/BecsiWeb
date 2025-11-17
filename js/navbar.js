4db6213091d16605d66e806aabe3effd4396cba6
Bl4ver <139627030+Bl4ver@users.noreply.github.com>
2025-11-17 21:55:45 +0100

    File reset - first try

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        if (navLinks.style.display === "flex") {
+            navLinks.style.display = "none";
+        } else {
+            navLinks.style.display = "flex";
+        }
+    });
+});
