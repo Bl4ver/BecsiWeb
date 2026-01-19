3c9afe4bdcc8c869481456c3bf0b2164365b9138
Bl4ver <bazsopatrik07@gmail.com>
2026-01-19 16:51:04 +0100

    Code újragondolás, responsivitás

--- js/navbar.js
+++ js/navbar.js
@@ -10,3 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+
+    navLinks.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", () => {
+            navLinks.classList.remove("active");
+        });
+    });
+});
